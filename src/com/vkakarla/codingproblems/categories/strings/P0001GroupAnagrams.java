5902fd5e2185b5a9c8b4e3cfa3856fc7305b2ce6
veerr <kakarlaveerraju2010@gmail.com>
2024-11-26 09:20:46 +0530

    V

--- src/com/vkakarla/codingproblems/categories/strings/P0001GroupAnagrams.java
+++ src/com/vkakarla/codingproblems/categories/strings/P0001GroupAnagrams.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 
 public class P0001GroupAnagrams {
@@ -12,7 +13,11 @@ public class P0001GroupAnagrams {
 		
 		String[] words = { "ace", "cea", "crew", "wrec", "werc", "yo", "oy", "act", "cat", "tac", "foo", "oof", "ofo","flop", "olfp" };
 		List<String> wordList = Arrays.asList(words);
-		List<List<String>> result = groupAnagrams(wordList);
+		//List<List<String>> result = groupAnagrams(wordList);
+		
+		
+		List<List<String>> result = groupAnagrams1(words);
+		
 		// result.forEach(t -> System.out.println(t + " "));
 		// OR
 		for (List<String> list : result) {
@@ -41,4 +46,25 @@ public class P0001GroupAnagrams {
 		result.addAll(map.values());
 		return result;
 	}
+	
+	
+	public static List<List<String>> groupAnagrams1(String[] strs) {
+        if (strs == null || strs.length == 0) return new ArrayList<>();
+        
+        Map<String, List<String>> map = new HashMap<>();
+        for (String s : strs) {
+            // Sort the string to get the key
+            char[] chars = s.toCharArray();
+            Arrays.sort(chars);
+            String key = new String(chars);
+            
+            // Add to the appropriate group
+            map.putIfAbsent(key, new ArrayList<>());
+            map.get(key).add(s);
+        }
+        
+        return new ArrayList<>(map.values());
+    }
+	
+	
 }
