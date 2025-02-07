d0ac3e112b8708f4290bd25376682e4aa0e63905
veerr <kakarlaveerraju2010@gmail.com>
2025-02-08 04:25:05 +0530

    Veer: Java Anagrams Solution1

---
+++ src/com/vkakarla/codingproblems/hackerRank/strings/AnagramTwoStrings.java
@@ -0,0 +1,38 @@
+package com.vkakarla.codingproblems.hackerRank.strings;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class AnagramTwoStrings {
+
+	public static void main(String[] abc) {
+
+		Scanner scan = new Scanner(System.in);
+		String a = scan.next();
+		String b = scan.next();
+		scan.close();
+
+		boolean result = isAnagram(a, b);
+
+		System.out.println(result ? "Anagrams" : "Not Anagrams");
+
+	}
+
+	public static boolean isAnagram(String a, String b) {
+
+		if (a.length() != b.length()) {
+			return false;
+		}
+		a.toLowerCase();
+		b.toLowerCase();
+
+		char[] ar1 = a.toCharArray();
+		char[] ar2 = b.toCharArray();
+
+		Arrays.sort(ar1);
+		Arrays.sort(ar2);
+
+		return Arrays.equals(ar1, ar2);
+
+	}
+}
